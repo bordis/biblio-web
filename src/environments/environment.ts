714259573659b6bc8e7a3a3e3019c894ab44f488
bordis <bordignonjm@gmail.com>
2024-06-29 13:18:37 -0300

    ajustes troca de nome

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
-
     production: true,
-    apiUrl: 'http://localhost:8080/biblio/api',
+    apiUrl: 'https://api.index.bib.br/biblio/api',
     siteKeyV3: '6LeEJvgpAAAAAFuz1zAKJQmUmQMSZ8o-rBCvKrdg'
-
 };
